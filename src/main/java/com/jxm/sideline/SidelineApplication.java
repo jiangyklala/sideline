5a7ecfd626c3927be49092d2c428e429d9ba8202
jiangyk <jiangyk@nwafu.edu.cn>
2024-05-26 15:30:02 +0800

    init

---
+++ src/main/java/com/jxm/sideline/SidelineApplication.java
@@ -0,0 +1,13 @@
+package com.jxm.sideline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jxm/sideline/SidelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jxm.sideline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
